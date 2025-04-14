9ea43af4a9c2c7b7973d06ed0a7c1114d451f2e5
kirill555525 <surskillwill@gmail.com>
2025-04-15 01:26:09 +0500

    fix

--- internal/compress/gzip.go
+++ internal/compress/gzip.go
@@ -69,17 +69,17 @@ func GzipMiddleware(next http.Handler) http.Handler {
 		//
 		//contentType := r.Header.Get("Content-Type")
 
-		if strings.Contains(r.Header.Get("Content-Type"), `text/html`) || strings.Contains(r.Header.Get("Content-Type"), `application/json`) {
-			if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
-				body, err := newCompressReader(r.Body)
-				if err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-				r.Body = body
-				defer body.Close()
+		//if strings.Contains(r.Header.Get("Content-Type"), `text/html`) || strings.Contains(r.Header.Get("Content-Type"), `application/json`) {
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			body, err := newCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
 			}
+			r.Body = body
+			defer body.Close()
 		}
+		//}
 
 		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			cw := newCompressWriter(w)
