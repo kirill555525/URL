d4dba4dd86fea8f5c41d018ea4e1eb95cab1114d
kirill555525 <surskillwill@gmail.com>
2025-04-17 19:31:38 +0500

    feat: connect db

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"os"
 )
 
@@ -10,15 +11,21 @@ type Config struct {
 	BaseURL         string // базовый адрес результирующего URL
 	FlagLogLevel    string
 	FileStoragePath string
+	DatabaseDSN     string
 }
 
 var cfg *Config
 
 func Init() *Config {
+
+	ps := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		`localhost`, `5432`, `postgres`, `postgres`, `postgres`)
+
 	addr := flag.String("a", "localhost:8080", "HTTP server address (e.g., localhost:8080)")
 	baseURL := flag.String("b", "http://localhost:8080", "Base URL for short links (e.g., http://localhost:8080/)")
 	flagLogLevel := flag.String("l", "info", "Log level (debug, info, warn, error, fatal)")
 	flagFileStoragePath := flag.String("f", "/tmp/short-url-db.json", "File storage path")
+	flagDatabaseDSN := flag.String("d", ps, "Database DSN")
 
 	flag.Parse()
 
@@ -36,6 +43,10 @@ func Init() *Config {
 
 	if envFileStoragePath := os.Getenv("FILE_STORAGE_PATH"); envFileStoragePath != "" {
 		*flagFileStoragePath = envFileStoragePath
+	}
+
+	if envDatabaseDSN := os.Getenv("DATABASE_DSN"); envDatabaseDSN != "" {
+		*flagDatabaseDSN = envDatabaseDSN
 	}
 
 	cfg = &Config{
